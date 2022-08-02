ab67ba486841d5cdcbd31913bbc1e96ca5e1d3de
Henry-Carr <henry.carr@nextfriday.co.uk>
2022-08-02 22:07:40 +0100

    updating feature format in app.py

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 
 def drawing():
-   
+
+   #feature = [typelist, subtype, offset, length, height1, height2, inward, side, inneroffset, name, leadingto]
+
    year_built = 2022
    box = [20,20]
    #features = []
-   features = [[0, 0, 0, 15, 5, 0, 5, 0, 0, "triangle feature thing1", ""]]
-   """features = [[0, 0, 0, 70, 7, 7, 7, 0, 0, "triangle feature thing1", ""]
-              ,[0, 0, 0, 50, 5, 5, 5, 0, 0, "triangle feature thing2", ""]
-              ,[0, 0, 0, 30, 3, 3, 3, 0, 0, "triangle feature thing3", ""]
-              ,[0, 0, 0, 10, 1, 1, 1, 0, 0, "triangle feature thing4", ""]]"""
+   features = [[0, 0, 15, 5, 0, 5, 0, 0, 0, "triangle feature thing1", ""]
+              ,[0, 0, 19, 2, 0, 2, 0, 0, 0, "triangle feature thing2", ""]
+              ,[0, 0, 16, 2, 2, 0, 0, 0, 0, "triangle feature thing2", ""]]
+
+   """
+   features = [[0, 0, 70, 5, 0, 5, 0, 0, 0, "triangle feature thing1", ""]
+              ,[0, 0, 50, 5, 0, 5, 0, 0, 0, "triangle feature thing2", ""]
+              ,[0, 0, 30, 5, 0, 5, 0, 0, 0, "triangle feature thing3", ""]
+              ,[0, 0, 10, 5, 0, 5, 0, 0, 0, "triangle feature thing4", ""]]"""
    js_data = start_the_programe(box,features,year_built)
    print(js_data)
    return render_template('drawing.html', value=js_data)
